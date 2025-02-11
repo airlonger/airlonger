b3d2061bfeb3c2fef9b0522c709fdf6b552a2204
airlonger <airlonger.com@gmail.com>
2025-02-11 12:06:59 -0500

    i added a page to record activities

---
+++ help.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Help Page</title>
+</head>
+<body>
+    <h1>Help Page</h1>
+    <p>This is where we record what we are all doing</p>
+    
+</body>
+</html>
